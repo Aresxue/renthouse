b7d772ae429264de676cc3688059b022dc554fab
薛冰 <wb-xb792319@alibaba-inc.com>
2020-12-10 13:20:08 +0800

    style: 换用风格代码

--- base-expand/src/main/java/com/asiainfo/frame/utils/SpringUtil.java
+++ base-expand/src/main/java/com/asiainfo/frame/utils/SpringUtil.java
@@ -1,5 +1,9 @@
 package com.asiainfo.frame.utils;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
@@ -9,11 +13,6 @@ import org.springframework.core.env.StandardEnvironment;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
 
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-import java.util.Properties;
-
 /**
  * @author: Ares
  * @date: 2019/5/31 16:18
@@ -21,18 +20,17 @@ import java.util.Properties;
  * @version: JDK 1.8
  */
 @Component
-public class SpringUtil implements ApplicationContextAware
-{
+public class SpringUtil implements ApplicationContextAware {
+
     private static ApplicationContext applicationContext;
 
     @Override
-    public void setApplicationContext(@NonNull ApplicationContext applicationContext) throws BeansException
-    {
+    public void setApplicationContext(@NonNull ApplicationContext applicationContext)
+        throws BeansException {
         SpringUtil.applicationContext = applicationContext;
     }
 
-    private static ApplicationContext getApplicationContext()
-    {
+    private static ApplicationContext getApplicationContext() {
         return applicationContext;
     }
 
@@ -43,8 +41,7 @@ public class SpringUtil implements ApplicationContextAware
      * @param: [beanName] Bean的名称
      * @return: java.lang.Object Bean
      */
-    public static Object getBean(String beanName)
-    {
+    public static Object getBean(String beanName) {
         return getApplicationContext().getBean(beanName);
     }
 
@@ -55,8 +52,7 @@ public class SpringUtil implements ApplicationContextAware
      * @param: [clazz] class
      * @return: T Bean
      */
-    public static <T> T getBean(Class<T> clazz)
-    {
+    public static <T> T getBean(Class<T> clazz) {
         return getApplicationContext().getBean(clazz);
     }
 
@@ -64,12 +60,10 @@ public class SpringUtil implements ApplicationContextAware
      * @author: Ares
      * @description: 通过class获取Bean
      * @date: 2019/5/31 16:23
-     * @param: [beanName, clazz]
-     * Bean名称, class
+     * @param: [beanName, clazz] Bean名称, class
      * @return: T Bean
      */
-    public static <T> T getBean(String beanName, Class<T> clazz)
-    {
+    public static <T> T getBean(String beanName, Class<T> clazz) {
         return getApplicationContext().getBean(beanName, clazz);
     }
 
@@ -80,8 +74,7 @@ public class SpringUtil implements ApplicationContextAware
      * @param: [beanName] Bean的名称
      * @return: boolean true为包含
      */
-    public static boolean containsBean(String beanName)
-    {
+    public static boolean containsBean(String beanName) {
         return getApplicationContext().containsBean(beanName);
     }
 
@@ -92,8 +85,7 @@ public class SpringUtil implements ApplicationContextAware
      * @param: [beanName] Bean的名称
      * @return: boolean true为是单例
      */
-    public static boolean isSingleton(String beanName)
-    {
+    public static boolean isSingleton(String beanName) {
         return getApplicationContext().isSingleton(beanName);
     }
 
@@ -104,8 +96,7 @@ public class SpringUtil implements ApplicationContextAware
      * @param: [beanName] Bean的名称
      * @return: java.lang.Class Class
      */
-    public static Class getType(String beanName)
-    {
+    public static Class getType(String beanName) {
         return getApplicationContext().getType(beanName);
     }
 
@@ -116,8 +107,7 @@ public class SpringUtil implements ApplicationContextAware
      * @param: [clazz] 请求参数
      * @return: java.util.Map<java.lang.String
      */
-    public static <T> Map<String, T> getBeansOfType(Class<T> clazz)
-    {
+    public static <T> Map<String, T> getBeansOfType(Class<T> clazz) {
         return getApplicationContext().getBeansOfType(clazz);
     }
 
@@ -128,12 +118,10 @@ public class SpringUtil implements ApplicationContextAware
      * @param: [] 请求参数
      * @return: java.util.List<java.lang.Class ?>> 响应参数
      */
-    public static List<Class<?>> getAllBeans()
-    {
+    public static List<Class<?>> getAllBeans() {
         List<Class<?>> result = new LinkedList<>();
         String[] beans = getApplicationContext().getBeanDefinitionNames();
-        for (String beanName : beans)
-        {
+        for (String beanName : beans) {
             Class<?> clazz = getType(beanName);
             result.add(clazz);
         }
@@ -144,12 +132,10 @@ public class SpringUtil implements ApplicationContextAware
      * @author: Ares
      * @description: 根据前缀获取spring的配置信息(不保留前缀)
      * @date: 2020/3/19 0:15
-     * @param: [environment, prefix]
-     * 环境信息实体, 前缀
+     * @param: [environment, prefix] 环境信息实体, 前缀
      * @return: java.util.Properties 响应参数
      */
-    public static Properties getPropertiesByPrefix(Environment environment, String prefix)
-    {
+    public static Properties getPropertiesByPrefix(Environment environment, String prefix) {
         return getPropertiesByPrefix(environment, prefix, false);
     }
 
@@ -157,21 +143,18 @@ public class SpringUtil implements ApplicationContextAware
      * @author: Ares
      * @description: 根据前缀获取spring的配置信息
      * @date: 2020/3/19 0:15
-     * @param: [environment, prefix, retainPrefix]
-     * 环境信息实体, 前缀, 是否保留前缀
+     * @param: [environment, prefix, retainPrefix] 环境信息实体, 前缀, 是否保留前缀
      * @return: java.util.Properties 响应参数
      */
-    public static Properties getPropertiesByPrefix(Environment environment, String prefix, boolean retainPrefix)
-    {
+    public static Properties getPropertiesByPrefix(Environment environment, String prefix,
+        boolean retainPrefix) {
         Properties properties = new Properties();
         StandardEnvironment standardEnvironment = (StandardEnvironment) environment;
         standardEnvironment.getPropertySources().forEach(propertySource -> {
-            if (propertySource instanceof MapPropertySource)
-            {
+            if (propertySource instanceof MapPropertySource) {
                 MapPropertySource mapPropertySource = (MapPropertySource) propertySource;
                 mapPropertySource.getSource().forEach((key, value) -> {
-                    if (key.startsWith(prefix))
-                    {
+                    if (key.startsWith(prefix)) {
                         String propertyKey = retainPrefix ? key : key.replace(prefix + ".", "");
                         properties.put(propertyKey, value);
                         properties.setProperty(propertyKey, String.valueOf(value));
@@ -186,12 +169,10 @@ public class SpringUtil implements ApplicationContextAware
      * @author: Ares
      * @description: 容器启动完成后在运行时根据前缀获取spring的配置信息
      * @date: 2020/3/19 0:15
-     * @param: [prefix, retainPrefix]
-     * 前缀, 是否保留前缀
+     * @param: [prefix, retainPrefix] 前缀, 是否保留前缀
      * @return: java.util.Properties 响应参数
      */
-    public static Properties getPropertiesByPrefixOnRuntime(String prefix, boolean retainPrefix)
-    {
+    public static Properties getPropertiesByPrefixOnRuntime(String prefix, boolean retainPrefix) {
         return getPropertiesByPrefix(applicationContext.getEnvironment(), prefix, retainPrefix);
     }
 
@@ -199,12 +180,10 @@ public class SpringUtil implements ApplicationContextAware
      * @author: Ares
      * @description: 容器启动完成后在运行时根据前缀获取spring的配置信息(不保留前缀)
      * @date: 2020/3/19 0:15
-     * @param: [prefix]
-     * 前缀
+     * @param: [prefix] 前缀
      * @return: java.util.Properties 响应参数
      */
-    public static Properties getPropertiesByPrefixOnRuntime(String prefix)
-    {
+    public static Properties getPropertiesByPrefixOnRuntime(String prefix) {
         return getPropertiesByPrefixOnRuntime(prefix, false);
     }
 
